4a49bdc0d5522a6a3948cb3d9388bbaec74c31bf
Vojtěch Vokoun <vojtech.vokoun@icloud.com>
2024-03-11 14:40:46 +0100

    Code reformat (Prettier)

--- src/webparts/workMinders/tools/suggestionApiCalls.ts
+++ src/webparts/workMinders/tools/suggestionApiCalls.ts
@@ -1,85 +1,85 @@
-import { WebPartContext } from "@microsoft/sp-webpart-base";
-import { MSGraphClientV3 } from "@microsoft/sp-http";
-import { TUserSuggestion, TTeamSuggestion } from "./suggestionApiCallsTypes";
+import {WebPartContext} from "@microsoft/sp-webpart-base";
+import {MSGraphClientV3} from "@microsoft/sp-http";
+import {TUserSuggestion, TTeamSuggestion} from "./suggestionApiCallsTypes";
 
 /**
  * This calls the Graph API to get the user autocomplete suggestions for a given search query.
  */
 export const getUserSuggestions = async (
-  context: WebPartContext,
-  query: string,
+    context: WebPartContext,
+    query: string,
 ): Promise<TUserSuggestion[]> => {
-  // If the query is too short, return an empty array
-  if (query.length < 3) {
-    return [];
-  }
+    // If the query is too short, return an empty array
+    if (query.length < 3) {
+        return [];
+    }
 
-  // Set up the Graph client
-  const client: MSGraphClientV3 =
-    await context.msGraphClientFactory.getClient("3");
+    // Set up the Graph client
+    const client: MSGraphClientV3 =
+        await context.msGraphClientFactory.getClient("3");
 
-  // Get the user suggestions
-  const userSuggestions = await client
-    .api(
-      `/users?$filter=startswith(displayName,'${query}') or startswith(mail,'${query}') or startswith(userPrincipalName,'${query}')`,
-    )
-    .version("v1.0")
-    .top(5)
-    .get();
+    // Get the user suggestions
+    const userSuggestions = await client
+        .api(
+            `/users?$filter=startswith(displayName,'${query}') or startswith(mail,'${query}') or startswith(userPrincipalName,'${query}')`,
+        )
+        .version("v1.0")
+        .top(5)
+        .get();
 
-  // TODO: remove after testing
-  console.log(userSuggestions);
+    // TODO: remove after testing
+    console.log(userSuggestions);
 
-  // Return the suggestions
-  return userSuggestions.value;
+    // Return the suggestions
+    return userSuggestions.value;
 };
 
 /**
  * This calls the Graph API to get the user's manager. This is used in the webpart after entering a certain keyword.
  */
 export const getManager = async (
-  context: WebPartContext,
+    context: WebPartContext,
 ): Promise<TUserSuggestion> => {
-  // Set up the Graph client
-  const client: MSGraphClientV3 =
-    await context.msGraphClientFactory.getClient("3");
+    // Set up the Graph client
+    const client: MSGraphClientV3 =
+        await context.msGraphClientFactory.getClient("3");
 
-  // Get the user's manager
-  const manager = await client.api("/me/manager").version("v1.0").get();
+    // Get the user's manager
+    const manager = await client.api("/me/manager").version("v1.0").get();
 
-  // TODO: remove after testing
-  console.log(manager);
+    // TODO: remove after testing
+    console.log(manager);
 
-  // Return the manager
-  return manager;
+    // Return the manager
+    return manager;
 };
 
 /**
  * This calls the Graph API to get suggestions for the user's Teams based on the user's input.
  */
 export const getTeamSuggestions = async (
-  context: WebPartContext,
-  query: string,
+    context: WebPartContext,
+    query: string,
 ): Promise<TTeamSuggestion[]> => {
-  // If the query is too short, return an empty array
-  if (query.length < 3) {
-    return [];
-  }
+    // If the query is too short, return an empty array
+    if (query.length < 3) {
+        return [];
+    }
 
-  // Set up the Graph client
-  const client: MSGraphClientV3 =
-    await context.msGraphClientFactory.getClient("3");
+    // Set up the Graph client
+    const client: MSGraphClientV3 =
+        await context.msGraphClientFactory.getClient("3");
 
-  // Get the team suggestions
-  const teamSuggestions = await client
-    .api(`/me/joinedTeams?$filter=startswith(displayName,'${query}')`)
-    .version("v1.0")
-    .top(5)
-    .get();
+    // Get the team suggestions
+    const teamSuggestions = await client
+        .api(`/me/joinedTeams?$filter=startswith(displayName,'${query}')`)
+        .version("v1.0")
+        .top(5)
+        .get();
 
-  // TODO: remove after testing
-  console.log(teamSuggestions);
+    // TODO: remove after testing
+    console.log(teamSuggestions);
 
-  // Return the suggestions
-  return teamSuggestions.value;
+    // Return the suggestions
+    return teamSuggestions.value;
 };
