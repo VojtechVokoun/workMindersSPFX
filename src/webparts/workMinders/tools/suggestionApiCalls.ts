231abd8c744f62a52fafa0373877ac4b97d8355c
Vojtěch Vokoun <vojtech.vokoun@icloud.com>
2024-03-11 16:00:57 +0100

    The Graph Client is now passed from the main func

--- src/webparts/workMinders/tools/suggestionApiCalls.ts
+++ src/webparts/workMinders/tools/suggestionApiCalls.ts
@@ -14,7 +14,7 @@ import {
  * This calls the Graph API to get the user autocomplete suggestions for a given search query.
  */
 export const getUserSuggestions = async (
-  context: WebPartContext,
+  client: MSGraphClientV3,
   query: string,
 ): Promise<TUserSuggestion[]> => {
   // If the query is too short, return an empty array
@@ -22,17 +22,13 @@ export const getUserSuggestions = async (
     return [];
   }
 
-  // Set up the Graph client
-  const client: MSGraphClientV3 =
-    await context.msGraphClientFactory.getClient("3");
-
   // Get the user suggestions
   const userSuggestions = await client
     .api(
       `/users?$filter=startswith(displayName,'${query}') or startswith(mail,'${query}') or startswith(userPrincipalName,'${query}')`,
     )
     .version("v1.0")
-    .top(5)
+    .select("id,displayName,mail,userPrincipalName")
     .get();
 
   // TODO: remove after testing
@@ -46,12 +42,8 @@ export const getUserSuggestions = async (
  * This calls the Graph API to get the user's manager. This is used in the webpart after entering a certain keyword.
  */
 export const getManager = async (
-  context: WebPartContext,
+  client: MSGraphClientV3,
 ): Promise<TUserSuggestion> => {
-  // Set up the Graph client
-  const client: MSGraphClientV3 =
-    await context.msGraphClientFactory.getClient("3");
-
   // Get the user's manager
   const manager = await client.api("/me/manager").version("v1.0").get();
 
@@ -66,7 +58,7 @@ export const getManager = async (
  * This calls the Graph API to get suggestions for the user's Teams based on the user's input.
  */
 export const getTeamSuggestions = async (
-  context: WebPartContext,
+  client: MSGraphClientV3,
   query: string,
 ): Promise<TTeamSuggestion[]> => {
   // If the query is too short, return an empty array
@@ -74,15 +66,11 @@ export const getTeamSuggestions = async (
     return [];
   }
 
-  // Set up the Graph client
-  const client: MSGraphClientV3 =
-    await context.msGraphClientFactory.getClient("3");
-
   // Get the team suggestions
   const teamSuggestions = await client
     .api(`/me/joinedTeams?$filter=startswith(displayName,'${query}')`)
     .version("v1.0")
-    .top(5)
+    .select("id,displayName,webUrl")
     .get();
 
   // TODO: remove after testing
