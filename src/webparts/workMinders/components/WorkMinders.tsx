c28d1d0a2fbce265abe94830528650b22f2d80e7
Vojtěch Vokoun <vojtech.vokoun@icloud.com>
2024-05-09 00:38:42 +0200

    Add tester functions and properties

--- src/webparts/workMinders/components/WorkMinders.tsx
+++ src/webparts/workMinders/components/WorkMinders.tsx
@@ -1,24 +1,79 @@
-import * as React from 'react';
-import styles from './WorkMinders.module.scss';
-import type { IWorkMindersProps } from './IWorkMindersProps';
-import { escape } from '@microsoft/sp-lodash-subset';
 import * as React from "react";
+import { useEffect } from "react";
 import styles from "./WorkMinders.module.scss";
 import type { IWorkMindersProps } from "./IWorkMindersProps";
+import { MSGraphClientV3 } from "@microsoft/sp-http";
+import {
+  //getManager,
+  getSites,
+  getTeamSuggestions,
+  getUserSuggestions,
+  getRecentFiles,
+} from "../tools/suggestionApiCalls";
 
-export default class WorkMinders extends React.Component<IWorkMindersProps, {}> {
-export default class WorkMinders extends React.Component<IWorkMindersProps> {
-  public render(): React.ReactElement<IWorkMindersProps> {
-    const { isDarkTheme, hasTeamsContext } = this.props;
+/**
+ * A background component that fetches data from the Graph API, renders the webpart and all its overlays.
+ * @param props
+ * @constructor
+ */
+const WorkMinders = (props: IWorkMindersProps): JSX.Element => {
+  //const [overlayActive, setOverlayActive] = React.useState<boolean>(true);
 
-    return (
-      <section
-        className={`${styles.va_welcomeImage} ${hasTeamsContext ? styles.teams : ""}`}
-      >
-        <div className={styles.va_welcome}>
-          nevim vole isDarkTheme: {isDarkTheme ? "true" : "false"}
-        </div>
-      </section>
-    );
-  }
-}
+  /**
+   * ! Test function
+   * Fetch all the data from the Graph API.
+   * @returns void
+   */
+  const getAll = async (): Promise<void> => {
+    // Generate the hraph client
+    const graphClient: MSGraphClientV3 =
+      await props.webpartContext.msGraphClientFactory.getClient("3");
+    //console.log("Manager:");
+    //await getManager(graphClient);
+    console.log("Team suggestions:");
+    await getTeamSuggestions(graphClient, "Coe");
+    console.log("User suggestions:");
+    await getUserSuggestions(graphClient, "Vojt");
+    console.log("Sites:");
+    await getSites(props.webpartContext);
+    console.log("Files:");
+    await getRecentFiles(graphClient);
+  };
+
+  // EFFECTS ----------------------------------------------
+  /**
+   * Fetch the data from the Graph API when the component is mounted.
+   */
+  useEffect(() => {
+    getAll().catch((error) => {
+      console.error("Error in useEffect: ", error);
+    });
+  }, []);
+
+  // STYLES -----------------------------------------------
+  const containerStyle = {
+    height: props.height,
+  };
+
+  // RENDER -----------------------------------------------
+  /**
+   * Render the webpart. If an overlay is active, render it as well (on top of the content).
+   */
+  return (
+    <div
+      className={`${styles.va_workMinders} ${props.hasTeamsContext ? styles.teams : ""} ${props.isDarkTheme ? styles.va_workMinders_dark : ""}`}
+      style={containerStyle}
+    >
+      {
+        //<div className={styles.va_screenOverlay} />
+      }
+
+      <div>nevim vole isDarkTheme: {props.isDarkTheme ? "true" : "false"}</div>
+      <div>
+        onedrive does not exist: {props.oneDriveDoesNotExist ? "true" : "false"}
+      </div>
+    </div>
+  );
+};
+
+export default WorkMinders;
---
+++ src/webparts/workMinders/loc/cs-cz.js
@@ -0,0 +1,27 @@
+define([], function () {
+  return {
+    // WORKMINDERS WEBPART
+    // Property Pane
+    propPaneDescription: "Nastavení",
+    // Property Pane - Look and Feel
+    propPaneLookAndFeel: "Vzhled a chování",
+    propPaneHeight: "Výška",
+    propPaneHeightDescription:
+      "Nastavte výšku WorkMinders. Výchozí hodnota je 500px.",
+
+    // Environment
+    AppLocalEnvironmentSharePoint:
+      "Aplikace běží ve vašem lokálním prostředí jako webová část SharePointu",
+    AppLocalEnvironmentTeams:
+      "Aplikace běží ve vašem lokálním prostředí jako aplikace Microsoft Teams",
+    AppLocalEnvironmentOffice:
+      "Aplikace běží ve vašem lokálním prostředí v office.com",
+    AppLocalEnvironmentOutlook:
+      "Aplikace běží ve vašem lokálním prostředí v Outlooku",
+    AppSharePointEnvironment: "Aplikace běží na stránce SharePointu",
+    AppTeamsTabEnvironment: "Aplikace běží v Microsoft Teams",
+    AppOfficeEnvironment: "Aplikace běží v office.com",
+    AppOutlookEnvironment: "Aplikace běží v Outlooku",
+    UnknownEnvironment: "Aplikace běží v neznámém prostředí",
+  };
+});
